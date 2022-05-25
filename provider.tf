55d258d7bacb77ea861b551e5b5c834b08b16c1c
Bob Adewusi <60587384+Bobaddey@users.noreply.github.com>
2022-05-25 03:42:38 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,11 @@
-
-
 terraform {
-  required_version = ">= 0.12.20"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
-
 
 provider "aws" {
   region                  = "us-east-1"
